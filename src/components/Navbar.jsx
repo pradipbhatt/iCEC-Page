7c160dd918469e4805a541f424d2167261277435
lakshita10341 <lakshitajainchopra89@gmail.com>
2024-10-17 23:55:06 +0530

    fix navbar issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
                 Communities
               </a>
             </li>
-            <li className="relative">
+            <li className="relative ">
               <div className="text-titleColor font-semibold transition-all cursor-pointer flex items-center gap-4 hover:text-hoverTextColor" onClick={() => toggleDropdown(2)}>
                 {/* SVG for Publications */}
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-notebook-pen">
@@ -145,7 +145,7 @@ const Navbar = () => {
             Our Team
             <FaCaretDown className="text-titleColor" />
           </button>
-          <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity z-10">
+         <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg hidden group-hover:block transition-opacity z-10">
             <li>
               <a href="/executive-committee" className="block px-4 py-2 text-base text-titleColor hover:bg-gray-100">
                 Executive Committee
@@ -166,7 +166,7 @@ const Navbar = () => {
             Publications
             <FaCaretDown className="text-titleColor" />
           </button>
-          <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity z-10">
+          <ul className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg hidden group-hover:block transition-opacity z-10">
             <li>
               <a href="/soe-express" className="block px-4 py-2 text-base text-titleColor hover:bg-gray-100">
                 SOE Express
